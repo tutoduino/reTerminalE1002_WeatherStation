e9c48e3c73a5c68611ca285bce65c31eb5f30cec
Tutoduino <tutoduino@gmx.fr>
2025-11-21 21:07:52 +0100

    Add precipitation to weather API URL

--- secrets.h
+++ secrets.h
@@ -3,6 +3,6 @@ const char* wifi_ssid = "xxx";
 const char* wifi_password = "xxx";
 const char* ha_url = "http://homeassistant.local:8123/api/states/";
 const char* ha_token = "xxx";
-const char* weather_url = "https://api.open-meteo.com/v1/forecast?latitude=35.75&longitude=12.3&daily=weather_code,temperature_2m_max,temperature_2m_min&current=weather_code,temperature";
+const char* weather_url = "https://api.open-meteo.com/v1/forecast?latitude=35.75&longitude=12.3&daily=weather_code,temperature_2m_max,temperature_2m_min&current=weather_code,temperature,precipitation";
 const char* btc_url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
 
